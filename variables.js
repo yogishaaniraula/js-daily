b176b36a5e65942077e4334b7e500499c7a5259a
Yogisha Niraula <yogishaniraula19@gmail.com>
2024-10-11 12:28:37 +0545

    variable rules

--- variables.js
+++ variables.js
@@ -1,3 +1,17 @@
-let name="egg";
-let roll =23;
-const pi =3.14;
+// Variables are contatiners for data.
+fullName ="Elizabeth";
+age =4;
+price = 34.23;
+a = null; //empty ( no value)
+e = undefined; // 
+isFollow = false; // ( either true or false) boolean type
+
+// Variable Rule
+// 1. variables are case sensitive.
+fullName ="Sam";
+FullName ="Sandy";   
+
+// only a letter, underscore(_),  $ should be 1st character.
+// no space between letters
+// reserved words cannot be variable names.
+
