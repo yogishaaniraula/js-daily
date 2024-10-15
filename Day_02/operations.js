080a73c169e581d45e29662e86ffa35f80c0faab
Yogisha Niraula <yogishaniraula19@gmail.com>
2024-10-15 21:27:05 +0545

    operations

--- Day_02/operations.js
+++ Day_02/operations.js
@@ -1,3 +1,29 @@
 let value = 3
 let negValue = -value
 console.log (negValue); // Output : -3
+
+
+// *********** Basic Operations *************** 
+console.log(2+2) // Output : 4
+console.log(2-2)// Output : 0
+console.log(2*2)// Output : 4
+console.log(2**3)// Output : 8
+console.log(2/3)// Output : 0.6666666666666666
+console.log(2%3)// Output : 2
+
+let str1 = "hello"
+let str2 = " yogisha"
+
+let str3 = str1 + str2
+console.log(str3) // output : hello yogisha
+
+console.log ("1" + 2) // output : 12
+console.log ( "1" + 2 +2) // output : 122
+console.log ( 1 + 2 + "2" ) // output : 32                         
+
+
+
+
+
+
+
