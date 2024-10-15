37a995caf2605a2a7cc7caddc1d546d571c57841
Yogisha Niraula <yogishaniraula19@gmail.com>
2024-10-15 21:01:49 +0545

    conversion

---
+++ Day_01/conversion.js
@@ -0,0 +1,11 @@
+let score  = 22
+ console.log ( typeof score) // output : number
+ console.log ( typeof (score))  // output : number
+
+ let word = "33"
+ console.log ( typeof word) // output : string
+ console.log ( typeof (word))  // output : string
+
+ let valueInNumber = Number(score)
+ console.log(typeof valueInNumber); // output number
+ console.log(valueInNumber); // output: NAN
