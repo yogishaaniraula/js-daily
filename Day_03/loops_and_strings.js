bc585accfb89f017f787b209e9906fc6405817ed
Yogisha Niraula <yogishaniraula19@gmail.com>
2024-10-16 20:16:52 +0545

    loops

---
+++ Day_03/loops_and_strings.js
@@ -0,0 +1,47 @@
+// loops are used to execute a program again and again.
+
+
+// 1. for loop 
+
+// 1.1  print 1 to 7
+
+for ( let count = 0; count <= 7; count ++)
+{
+    console.log("Yogisha Niraula") // output : Yogisha Niraula ( executed 7 times, couting start from 0-7)
+    
+}
+
+// 1.2 calculate sum 1 to 5
+
+let sum = 0;
+for ( let i =0 ; i <= 5 ;i++ )
+{
+    sum = sum + i
+    
+
+}
+console.log("the result = ", sum); // output : the result is 15
+
+console.log ("Your required sum is obtained.")
+
+
+// 2. while loop
+
+let i = 2;
+while ( i <=6)
+{
+    console.log("Hi, I am yogisha" ); // execute 5 times
+    i++;
+ 
+}
+
+
+// 3. do while loop
+
+let j = 1;
+do {
+    console.log("how are you?");
+    j++;
+} while (j<=5);
+
+
