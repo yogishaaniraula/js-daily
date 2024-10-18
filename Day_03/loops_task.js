47966e999cad5bc55c887b63bae70916de2914b1
Yogisha Niraula <yogishaniraula19@gmail.com>
2024-10-18 16:45:31 +0545

    day 4

---
+++ Day_03/loops_task.js
@@ -0,0 +1,14 @@
+// Q.1  Print all even number from 0 to 100
+
+let list;
+for ( let num =0 ; num <=100 ; num ++) 
+{
+    if ( num % 2 === 0)
+    {
+        console.log (num) // execute all even numbers
+    }
+   
+}
+
+
+
---
+++ Day_03/strings.js
@@ -0,0 +1,24 @@
+// String is a sequence of character used to represent text
+
+// create strings
+let str = "Avacado";
+let str2 ='orange';
+
+console.log(str, str2); // Output : avacado , orange
+
+
+
+// to find length of strings
+console.log ( str.length); // output : 7
+ 
+console.log ( str2.length); // output : 6
+
+
+
+// charcter position
+console.log ( str[0]); // output : A
+
+console.log ( str2 [4]); // output: g
+
+
+// Template literals
