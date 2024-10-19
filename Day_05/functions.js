b91f73b8516efee02099ca3135ea12d3dc7717e4
Yogisha Niraula <yogishaniraula19@gmail.com>
2024-10-19 21:31:27 +0545

    functions

---
+++ Day_05/functions.js
@@ -0,0 +1,26 @@
+function myfunction()
+{
+    console.log("welcome to my github");
+    console.log("this is yogisha niraula");
+}
+
+myfunction();
+
+
+function myfunction(msg)
+{
+    // inside function definition :parameter
+    console.log(msg)
+}
+
+myfunction("i love nepal") ; // inside function call : argument
+
+
+function sum ( x,y)
+{
+    s = x + y;
+    return s;
+}
+
+let val =sum(2,4);
+console.log(val);// output : 6
